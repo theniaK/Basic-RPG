7d1db0a5efa99e8dc60460149156e913cd24f473
theniaK <theniakoutrouli@gmail.com>
2018-03-16 13:26:03 +0200

    Add files via upload

---
+++ SignPost.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SignPost : ActionItem {
+
+    public string[] SignDialogue;
+    public string SignName;
+    public override void Interact()
+    {
+        DialogueSystem.Instance.AddNewDialogue(SignDialogue, SignName);
+        Debug.Log("Interacting with sign post");
+    }
+}
